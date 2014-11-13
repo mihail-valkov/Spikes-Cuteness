dd5a95948637957f860265fa9038419fbf453d41
Erjan Gavalji <erjan.gavalji@gmail.com>
2014-11-13 10:49:33 +0200

    Update app and modules to version 0.4.0

--- nativescript-sample-cuteness/app/main-page.js
+++ nativescript-sample-cuteness/app/main-page.js
@@ -1,73 +1,57 @@
-﻿var http = require("http");
-
-var pages = require("ui/pages");
-var listViewDef = require("ui/list-view");
-var tabDef = require("ui/tab-control");
-var virtualArrayDef = require("ui/core/virtual-array");
-
-var listViewItemDef = require("/list-view-item");
-var detailsPageDef = require("/details-page");
-
-var aboutPanelDef = require("/about-panel");
-
-var redditUrl = "http://www.reddit.com/r/aww.json?limit=";
-var after;
-
-var items = new virtualArrayDef.VirtualArray(1000);
-items.loadSize = 50;
-items.on(virtualArrayDef.knownEvents.itemsLoading, function (args) {
-    http.getJSON(redditUrl + args.count + (after ? "&after=" + after : "")).then(function (result) {
-        var itemsToLoad = result.data.children;
-
-        items.load(args.index, itemsToLoad);
-
-        var lastItem = itemsToLoad[itemsToLoad.length - 1];
-        if (lastItem) {
-            after = itemsToLoad[itemsToLoad.length - 1].data.name;
-        }
-    }).fail(function (e) {
-        console.log(e.message);
-    });
-    ;
-});
-
-var listView = new listViewDef.ListView();
-
-listView.on(listViewDef.knownEvents.itemLoading, function (args) {
+var view = require("ui/core/view");
+var imageSource = require("image-source");
+var frames = require("ui/frame");
+var listView = require("ui/list-view");
+var listItem = require("/list-view-item");
+var model = require("/view-model");
+var cache = global.imageCache;
+var defaultOnChanged = listView.isScrollingProperty.metadata.onValueChanged;
+listView.isScrollingProperty.metadata.onValueChanged = handleIsScrollingChanged;
+function handleIsScrollingChanged(data) {
+    if (defaultOnChanged) {
+        defaultOnChanged(data);
+    }
+    if (data.newValue) {
+        cache.disableDownload();
+    }
+    else {
+        cache.enableDownload();
+    }
+}
+function pageLoaded(args) {
+    var page = args.object;
+    if (frames.topmost().android) {
+        frames.topmost().android.cachePagesOnNavigate = true;
+    }
+    global.loadCss(page);
+    var feedListView = view.getViewById(page, "feedListView");
+    if (feedListView) {
+        feedListView.items = model.items;
+    }
+    var aboutImage = view.getViewById(page, "aboutImage");
+    if (aboutImage) {
+        aboutImage.source = imageSource.fromFile(__dirname + "/telerik-logo.png");
+    }
+}
+exports.pageLoaded = pageLoaded;
+function listViewItemLoading(args) {
+    var listView = args.object;
     var listViewItem = args.view;
     if (!listViewItem) {
-        listViewItem = new listViewItemDef.ListViewItem(listView);
+        listViewItem = new listItem.ListViewItem(listView);
         args.view = listViewItem;
     }
-
-    listViewItem.item = items.getItem(args.index);
-});
-
-listView.on(listViewDef.knownEvents.loadMoreItems, function (args) {
-    items.length += items.loadSize;
-});
-
-listView.on(listViewDef.knownEvents.itemTap, function (args) {
-    var detailsPage = new detailsPageDef.DetailsPage();
-    page.frame.navigate({
-        page: detailsPage,
-        context: items.getItem(args.index)
-    });
-});
-
-listView.items = items;
-
-var page = new pages.Page();
-
-if (page.android) {
-    page.android.cacheOnNavigatedFrom = true;
+    listViewItem.item = model.items.getItem(args.index);
 }
-
-var tabControl = new tabDef.TabControl();
-tabControl.items = [{ title: "List", view: listView }, { title: "About", view: new aboutPanelDef.AboutPanel() }];
-
-page.content = tabControl;
-
-global.loadCss(page);
-
-exports.Page = page;
+exports.listViewItemLoading = listViewItemLoading;
+function listViewItemTap(args) {
+    frames.topmost().navigate({
+        moduleName: "/details-page",
+        context: model.items.getItem(args.index)
+    });
+}
+exports.listViewItemTap = listViewItemTap;
+function listViewLoadMoreItems(args) {
+    model.items.length += model.items.loadSize;
+}
+exports.listViewLoadMoreItems = listViewLoadMoreItems;
